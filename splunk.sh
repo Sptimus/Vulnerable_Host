6e7a60e026d159d338d83111ba2ce659382b4417
Sptimus <54841399+Sptimus@users.noreply.github.com>
2022-10-26 18:40:22 +0200

    Update splunk.sh

--- splunk.sh
+++ splunk.sh
@@ -1,2 +1,26 @@
+echo " "
+# Setting Up default Tools
+
+cd ~/
+apt update
+apt install -y wget vim vim-python-jedi curl exuberant-ctags git ack-grep python3-pip git
+pip3 install pep8 flake8 pyflakes isort yapf Flask
+
+#Installing apache
+apt install -y apache2
+ufw allow 'Apache'
+
+echo "Installing Splunk and necessary dependencies!"
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+echo 'deb [arch=amd64] https://download.docker.com/linux/debian buster stable' | sudo tee /etc/apt/sources.list.d/docker.list
+apt update
+apt remove docker docker-engine docker.io
+apt install -y docker-ce
+wget https://raw.githubusercontent.com/Sptimus/Vulnerable_Host/main/env.example
+
+echo "Installing Updating Docker-Compose!"
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 docker run -d --mount source=logs-webgoat,target=/mnt/webgoat --mount source=logs-juice-shop,target=/mnt/juice-shop --mount source=logs-mutillidae_2,target=/mnt/mutillidae_2 --mount source=logs-dvwa,target=/mnt/dvwa --mount source=logs-hackazon,target=/mnt/hackazon -p 8000:8000 -p 8080:8080 -e "SPLUNK_START_ARGS=--accept-license" -e "SPLUNK_PASSWORD=Adam1000" --name splunk splunk/splunk:latest
 
+echo "Please now ADD your Splunk token to .env file in /root directory. Then run sudo ./install.sh"
