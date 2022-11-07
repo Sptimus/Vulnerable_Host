263b72769c987400798dfdc4eace3b988e3fbed1
Sptimus <54841399+Sptimus@users.noreply.github.com>
2022-11-07 14:39:04 +0100

    Delete splunk_install.sh

--- splunk_install.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-#Installing splunk
-docker run -d -p 8000:8000 -p 8080:8080 -e "SPLUNK_START_ARGS=--accept-license" -e "SPLUNK_PASSWORD=admin" --name splunk splunk/splunk:latest
